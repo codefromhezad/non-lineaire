f8e7ddf2de64da3b05e22a013926d1c30a53f188
Pierrick Varin <pierrick.varin@glitchstudio.com>
2015-11-16 23:13:40 +0100

    Updated 'distributions-aleatoires'

--- pages/distributions-aleatoires.php
+++ pages/distributions-aleatoires.php
@@ -19,7 +19,7 @@
 			Réciproquement, moins la grille est uniforme, plus il est probable que le processus de génération des samples soit biaisé.
 			<br>
 			<br>
-			La taille des échantillons sera pour chaque figure relativement faible afin de ne pas atteindre d'uniformité statistique tout ou pouvant visualiser la différence notable de rendu entre les différents processus.
+			La taille de chaque échantillon est relativement faible afin de pouvoir comparer des données légères tout ou pouvant visualiser la différence notable de rendu entre les différents processus.
 		</div>
 
 		<div class="item line-2">
@@ -29,9 +29,6 @@
 
 			<div class="equation">
 				Les chiffres ont été tapés "aléatoirement" sur un clavier d'ordinateur portable, la main gauche restant proche des chiffres entre 1 et 5, la main droite entre 5 et 0 ("0" se situant à droite du "9" sur le clavier).<br>
-				<br>
-				<strong>Suite utilisée :</strong><br>
-				<pre style="word-wrap: break-word;">49582967402957485739730194738695830698586738405927495860293857463940681039857684039485930095830385832020239585676729125446504393285809329258567329292845758192382347568394581235034684729359657394356934029183495823325</pre>
 			</div>
 		</div>
 
@@ -42,9 +39,6 @@
 
 			<div class="equation">
 				Les chiffres ont été à nouveau tapés "aléatoirement" sur un clavier d'ordinateur portable, la main gauche restant proche des chiffres entre 1 et 5, la main droite entre 5 et 0 ("0" se situant à droite du "9" sur le clavier).<br>
-				<br>
-				<strong>Suite utilisée :</strong><br>
-				<pre style="word-wrap: break-word;">948375759229458829139585403093478592023058239258230923193285859234855493485728101405867392302358568239239586850523023905482910932859302858239320191823857583935740329183475674393894829023757392303289328593938358695033202394873687289192385859394357592921</pre>
 			</div>
 		</div>
 
@@ -56,10 +50,7 @@
 			</div>
 
 			<div class="equation">
-				Les chiffres correspondent aux premières décimales de `pi`<br>
-				<br>
-				<strong>Suite utilisée :</strong><br>
-				<pre style="word-wrap: break-word;">14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344</pre>
+				Les chiffres correspondent aux ~280 premières décimales de `pi`<br>
 			</div>
 		</div>
 
@@ -69,10 +60,7 @@
 			</div>
 
 			<div class="equation">
-				Les chiffres correspondent aux premières décimales de `e`<br>
-				<br>
-				<strong>Suite utilisée :</strong><br>
-				<pre style="word-wrap: break-word;">718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059921817413596629043572900334295260595630738132328627943490763233829880753195251019011573834187930702154089149934884167509244761460668082264800168477411853742345442437107</pre>
+				Les chiffres correspondent aux ~280 premières décimales de `e`<br>
 			</div>
 		</div>
 		
@@ -84,10 +72,7 @@
 			</div>
 
 			<div class="equation">
-				Les caractères des deux premières strophes du <em>Bateau Ivre</em> d'Arthur Rimbaud sont d'abord convertis en entiers entre 1 et 26 (Les caractères non alphanumériques sont supprimés). Le modulo 10 de chaque nombre est ensuite utilisé pour générer la séquence correspondant à cette figure.<br>
-				<br>
-				<strong>Suite utilisée :</strong><br>
-				<pre style="word-wrap: break-word;">3533505459354419945962512599361999225905453595409962197194618259812518945965114851759389184925912195406899651839225925915140325194191146505114453512518900199949513951445051925971961759658051845229621314495145350549147219971144125335981251895406949359016175925962512593540219994593544855052512199</pre>
+				Les caractères des deux premières strophes du <em>Bateau Ivre</em> d'Arthur Rimbaud sont d'abord convertis en entiers entre 1 et 26 (Les caractères non alphanumériques sont supprimés). <br>Le modulo 10 de chaque nombre est ensuite utilisé pour générer la séquence correspondant à cette figure.<br>
 			</div>
 		</div>
 
@@ -97,10 +82,41 @@
 			</div>
 
 			<div class="equation">
-				Les caractères du poème <em>L'étranger</em> de Charles Baudelaire (extrait du <em>Spleen de Paris</em>) sont d'abord convertis en entiers entre 1 et 26 (Les caractères non alphanumériques sont supprimés). Le modulo 10 de chaque nombre est ensuite utilisé pour générer la séquence correspondant à cette figure.<br>
-				<br>
-				<strong>Suite utilisée :</strong><br>
-				<pre style="word-wrap: break-word;">7191935901253951485335497310971549905468501385019551851054688505419496854938549918496885059139925192519958256241456185254540259549359085900197135051894354410161089509745859519715225210901455225590990152125110052193581992525409589459955093358052252580525819935335251981995649515871193590145435408158494198508147580193525941175925941175971961995402219221925935825922514411759</pre>
+				Les caractères du poème <em>L'étranger</em> de Charles Baudelaire (extrait du <em>Spleen de Paris</em>) sont d'abord convertis en entiers entre 1 et 26 (Les caractères non alphanumériques sont supprimés). <br>Le modulo 10 de chaque nombre est ensuite utilisé pour générer la séquence correspondant à cette figure.<br>
+			</div>
+		</div>
+
+		<div style="clear:both;"></div>
+	</div>
+
+	<div class="serie">
+		<div class="item line-3">
+			<div class="item-img">
+				<img src="img/aleatoire0mul.png">
+			</div>
+
+			<div class="equation">
+				Les visuels correspondant aux séquences entrées au clavier sont superposés avec un mode de fusion "Mulitiplication" afin de mettre en évidence les zones plus denses dans ces deux cas
+			</div>
+		</div>
+
+		<div class="item line-3">
+			<div class="item-img">
+				<img src="img/aleatoire1mul.png">
+			</div>
+
+			<div class="equation">
+				Les visuels correspondant aux séquences générées à partir de constantes mathématiques sont superposés avec un mode de fusion "Mulitiplication" afin de mettre en évidence les zones plus denses dans ces deux cas
+			</div>
+		</div>
+
+		<div class="item line-3">
+			<div class="item-img">
+				<img src="img/aleatoire23mul.png">
+			</div>
+
+			<div class="equation">
+				Les visuels correspondant aux séquences générées à partir de textes écrits en français sont superposés avec un mode de fusion "Mulitiplication" afin de mettre en évidence les zones plus denses dans ces deux cas
 			</div>
 		</div>
 
